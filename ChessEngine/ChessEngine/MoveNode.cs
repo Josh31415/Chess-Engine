f7dd9eee92bdbc4b7df182ecb6931f338e961548
jobrud31415 <jobrud314@gmail.com>
2017-11-25 15:11:59 -0500

    Added move tree

---
+++ ChessEngine/ChessEngine/MoveNode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace ChessEngine
+{
+    class MoveNode
+    {
+        private pieceMove data;
+        private MoveNode parent;
+        private List<MoveNode> childNodes;
+
+        public MoveNode(MoveNode parent, pieceMove data, List<MoveNode> children)
+        {
+            this.parent = parent;
+            this.data = data;
+            this.childNodes = children;
+        }
+
+        public pieceMove Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public MoveNode Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+
+        public List<MoveNode> getChildNodes()
+        {
+            return childNodes;
+        }
+
+        public void setChildNodes(List<MoveNode> children)
+        {
+            this.childNodes = children;
+        }
+
+        public void addChild(MoveNode node)
+        {
+            childNodes.Add(node);
+        }
+
+        public void removeChild(MoveNode node)
+        {
+            childNodes.Remove(node);
+        }
+    }
+}
---
+++ ChessEngine/ChessEngine/MoveTree.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+
+namespace ChessEngine
+{
+    class MoveTree
+    {
+        MoveNode head;
+        MoveNode currNode;
+        int depth;
+
+        public MoveTree(pieceMove headData)
+        {
+            head = new MoveNode(null, headData, null);
+            depth = 0;
+        }
+
+        public int Depth
+        {
+            get { return depth; }
+            set { depth = value; }
+        }
+
+        public void addLevel(MoveNode parent, pieceMove[] data)
+        {
+            for(int i = 0; i < data.Length; i++)
+            {
+                parent.addChild(new MoveNode(parent, data[i], null));
+            }
+
+            depth++;
+        }
+
+        public void addCurrChildren(pieceMove[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                currNode.addChild(new MoveNode(currNode, data[i], null));
+            }
+        }
+
+        public List<MoveNode> getNextLevel()
+        {
+            return currNode.getChildNodes();
+        }
+
+        public bool moveDown(int index)
+        {
+            MoveNode[] nodes = currNode.getChildNodes().ToArray();
+            if(nodes.Length <= index && !nodes[index].Equals(null)) {
+                currNode = nodes[index];
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool moveRight()
+        {
+            int index = -1;
+            MoveNode par = currNode.Parent;
+            MoveNode[] nodes = par.getChildNodes().ToArray();
+
+            for(int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i] == currNode) index = i;
+            }
+
+            if(index != -1 && index > 0)
+            {
+                currNode = nodes[index - 1];
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool moveLeft()
+        {
+            int index = -1;
+            MoveNode par = currNode.Parent;
+            MoveNode[] nodes = par.getChildNodes().ToArray();
+
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i] == currNode) index = i;
+            }
+
+            if (index != -1 && index < nodes.Length)
+            {
+                currNode = nodes[index + 1];
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool moveUp()
+        {
+            if(!currNode.Parent.Equals(null))
+            {
+                this.currNode = currNode.Parent;
+            }
+
+            return false;
+        }
+    }
+}
